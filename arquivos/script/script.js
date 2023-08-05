c5cc436b23ed53608ef7ace6d3853721d13a7ade
samuelwesleydev <114779480+samuelwesleydev@users.noreply.github.com>
2023-08-05 14:21:56 -0300

    menu para mobile
    criei um menu simples

--- arquivos/script/script.js
+++ arquivos/script/script.js
@@ -1,3 +1,18 @@
+
+function ativarMenu() {
+    const menu = document.querySelector('#menu-de-links')
+
+    if (menu.style.display == "none") {
+        menu.style.display = "block"
+    }
+    else {
+        menu.style.display = "none"
+    }
+}
+
+
+
+
 function aviso(){
     window.alert(`Não funciona né! kkkk`)
 }
