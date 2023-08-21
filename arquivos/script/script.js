17f271837ef989439116d1a85ca27c083b9811c2
samuel <114779480+samuelwesleydev@users.noreply.github.com>
2023-08-21 14:18:35 -0300

    menu

--- arquivos/script/script.js
+++ arquivos/script/script.js
@@ -1,18 +1,45 @@
-
-function ativarMenu() {
-    const menu = document.querySelector('#menu-de-links')
-
-    if (menu.style.display == "none") {
-        menu.style.display = "block"
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
     }
-    else {
-        menu.style.display = "none"
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `linksFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
     }
-}
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".hamburguer",
+    "#menu-de-links",
+    "#menu-de-links li",
+  );
+  mobileNavbar.init();
 
-
-
-
-function aviso(){
-    window.alert(`Não funciona né! kkkk`)
-}
